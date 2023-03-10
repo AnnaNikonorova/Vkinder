64f179347328f30f47b49b17325bea7c539fdbb2
AnnaNikonorova <122745345+AnnaNikonorova@users.noreply.github.com>
2023-03-10 10:32:57 +0300

    Delete configuration.py

--- configuration.py
+++
@@ -1,3 +0,0 @@
-V = '5.131'
-group_token = 'vk1.a.ust9UIsQd3j0GPVr7lflv2vumng04Y1crG6kCoRo3zxD_Rdz6X11jKt73dekrCEpSSOq-N7X7AClu3OHA8xiW8uT2Fhe9JSJnZds8Mxi--NA7Bd0EVJBfPyU-wNVwAXVRmxQpUPI_CInaNiaLf0rMlgL2qVBgF_nfjJXHQ4KU_8yMOhnXlfBFid7pjlfcZ0ZVag-WSCq25OPCj2JsQYvLw'
-user_token = input('Token: ')
